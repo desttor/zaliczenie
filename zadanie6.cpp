90362f7c28f226cec5eeade7cc8f6869852bdfe7
Oskar <oskar.czuchaj@outlook.com>
2025-01-17 22:48:22 +0100

    dodano pliki zrodlowe

---
+++ zadanie6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Osoba {
+    string imie;
+    string nazwisko;
+    int wiek;
+};
+
+int main() {
+    Osoba osobaStatyczna;
+    osobaStatyczna.imie = "Jan";
+    osobaStatyczna.nazwisko = "Kowalski";
+    osobaStatyczna.wiek = 30;
+
+    cout << "Osoba statyczna: " << endl;
+    cout << "Imie: " << osobaStatyczna.imie << endl;
+    cout << "Nazwisko: " << osobaStatyczna.nazwisko << endl;
+    cout << "Wiek: " << osobaStatyczna.wiek << endl;
+
+    Osoba* osobaDynamiczna = new Osoba;
+
+    osobaDynamiczna->imie = "Anna";
+    osobaDynamiczna->nazwisko = "Nowak";
+    osobaDynamiczna->wiek = 25;
+
+    cout << "\nOsoba dynamiczna: " << endl;
+    cout << "Imie: " << osobaDynamiczna->imie << endl;
+    cout << "Nazwisko: " << osobaDynamiczna->nazwisko << endl;
+    cout << "Wiek: " << osobaDynamiczna->wiek << endl;
+
+    delete osobaDynamiczna;
+
+    return 0;
+}
---
+++ zadanie7.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+// Enum (wyliczenie) - pozwala zdefiniowac zestaw nazwanych stalych liczbowych.
+// Ulatwia prace z wartosciami symbolicznymi zamiast liczb.
+enum Dzien {
+    Poniedzialek,
+    Wtorek,    
+    Sroda,      
+    Czwartek, 
+    Piatek,
+    Sobota, 
+    Niedziela
+};
+
+// Unia - struktura pamieciowa, ktora pozwala przechowywac rozne typy danych w tym samym miejscu w pamieci.
+// Wszystkie pola unii wspoldziela te sama przestrzen pamieci, dlatego w danym momencie mozna uzywac tylko jednego pola.
+union Dane {
+    int liczbaCalkowita;
+    float liczbaZmiennoprzecinkowa;
+    char znak;
+};
+
+int main() {
+    // Uzycie enum
+    Dzien dzisiaj = Piatek;
+
+    cout << "Dzisiaj jest: ";
+    switch (dzisiaj) {
+    case Poniedzialek: cout << "Poniedzialek"; break;
+    case Wtorek: cout << "Wtorek"; break;
+    case Sroda: cout << "Sroda"; break;
+    case Czwartek: cout << "Czwartek"; break;
+    case Piatek: cout << "Piatek"; break;
+    case Sobota: cout << "Sobota"; break;
+    case Niedziela: cout << "Niedziela"; break;
+    }
+    cout << endl;
+
+    // Uzycie unii
+    Dane mojeDane;
+    mojeDane.liczbaCalkowita = 42;
+
+    cout << "Liczba calkowita: " << mojeDane.liczbaCalkowita << endl;
+
+    mojeDane.liczbaZmiennoprzecinkowa = 3.14f;
+    cout << "Liczba zmiennoprzecinkowa: " << mojeDane.liczbaZmiennoprzecinkowa << endl;
+
+    mojeDane.znak = 'A';
+    cout << "Znak: " << mojeDane.znak << endl;
+
+    return 0;
+}
